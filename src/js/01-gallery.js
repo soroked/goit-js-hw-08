92318632a21b737f033f88a02598c9efc22df7ec
Eduard <sorokolietov@gmail.com>
2023-08-25 13:03:56 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+// to remove bulletpoints of li elements
+galleryRef.querySelector('.gallery__item').style.listStyle = 'none';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(arr) {
+  return arr.map(item => 
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}" 
+          alt="${item.description}">
+      </a>
+    </li>`
+  ).join('');
+};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,6 @@
 
+
+export const five = 5;
+const def = 10
+
+export default def;
