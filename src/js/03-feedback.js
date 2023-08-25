c87ea5deb419267c37ba36232871b339683f60aa
Eduard <sorokolietov@gmail.com>
2023-08-25 15:37:23 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const FORM_KEY = "feedback-form-state";
 
+const formRef = document.querySelector('.feedback-form');
+const obj = JSON.parse(localStorage.getItem(FORM_KEY)) ?? {};
+
+// setting the values of the form, in case they exist
+for (key in obj) {
+  formRef[key].value = obj[key];
+}
+
+// Listener on formRef input and submit
+formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', onFormSubmit);
+
+// func-handler
+function onFormInput(e) {
+
+  const formName = e.target;
+
+  obj[formName.name] = formName.value;
+
+  localStorage.setItem(FORM_KEY, JSON.stringify(obj));
+};
+
+// onSubmit
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  for (key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+  }
+
+  // LS reset
+  localStorage.clear();
+
+  // form reset
+  e.target.reset();
+};
