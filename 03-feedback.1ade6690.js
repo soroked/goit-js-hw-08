ada0746827a0e9446e13345a96afcd9da51e6459
soroked <66534083+soroked@users.noreply.github.com>
2023-08-25 15:38:00 +0000

    Deploying to gh-pages from @ soroked/goit-js-hw-08@0316014e81036a51058c839661a66e4f7b87b03f 🚀

---
+++ 03-feedback.1ade6690.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");var t;const o=null!==(t=JSON.parse(localStorage.getItem("feedback-form-state")))&&void 0!==t?t:{};for(const t in o)e[t].value=o[t];e.addEventListener("input",(function(e){const t=e.target;o[t.name]=t.value,localStorage.setItem("feedback-form-state",JSON.stringify(o))})),e.addEventListener("submit",(function(e){e.preventDefault(),console.log(o);for(const e in o)delete o[e];localStorage.clear(),e.target.reset()}));
+//# sourceMappingURL=03-feedback.1ade6690.js.map
